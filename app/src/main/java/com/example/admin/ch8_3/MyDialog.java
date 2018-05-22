b3454c1ff0dc824f8a2d0c174a0abd8c66e3d725
chenyirou <annie851216@gmail.com>
2018-05-22 09:55:43 +0800

    MyDialog繼承DialogFragment，並宣告靜態方法

--- app/src/main/java/com/example/admin/ch8_3/MyDialog.java
+++ app/src/main/java/com/example/admin/ch8_3/MyDialog.java
@@ -2,6 +2,7 @@ package com.example.admin.ch8_3;
 
 
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,7 +12,14 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyDialog extends Fragment {
+public class MyDialog extends DialogFragment {
+    static MyDialog newInstance(String title){
+        MyDialog dlg = new MyDialog();
+        Bundle args = new Bundle();
+        args.putString("title" , title);
+        dlg.setArguments(args);
+        return dlg;
+    }
 
 
     public MyDialog() {
