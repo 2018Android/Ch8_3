a4c65e4d2bde65da4d1ef585f9e9c5d6ec0d132e
chenyirou <annie851216@gmail.com>
2018-05-22 12:29:20 +0800

    "輸入對話方塊" test OK

--- app/src/main/java/com/example/admin/ch8_3/EditNameDialog.java
+++ app/src/main/java/com/example/admin/ch8_3/EditNameDialog.java
@@ -8,24 +8,57 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
+
+/*
+interface EditNameDialogListener{
+    void onFinishEditDialog(String inputText);
+}
+MainActivity就不需要
+public void onFinishEditDialog(String inputText){
+        Toast.makeText(this, "您好,"+inputText, Toast.LENGTH_SHORT).show();
+}
+*/
+
 public class EditNameDialog extends DialogFragment {
+    private EditText edt_name;
+    //定義一個介面"EditNameDialogListener" 包含 "onFinishEditDialog"方法
+    public interface EditNameDialogListener{
+        void onFinishEditDialog(String inputText);
+    }
 
 
     public EditNameDialog() {
         // Required empty public constructor
     }
 
+    View.OnClickListener btn_OK_click = new View.OnClickListener(){
+      public void onClick(View v){
+          EditNameDialogListener activity = (EditNameDialogListener)getActivity();
+          //MainActivity activity = (MainActivity)getActivity();
+          activity.onFinishEditDialog(edt_name.getText().toString());
+          EditNameDialog.this.dismiss();
+      }
+    };
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_name_dialog, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_name_dialog, container, false);
+        getDialog().setTitle("請輸入姓名");
+        edt_name = (EditText)view.findViewById(R.id.edt_name);
+        edt_name.requestFocus();
+        Button btn_OK = (Button)view.findViewById(R.id.btn_ok);
+        btn_OK.setOnClickListener(btn_OK_click);
+        return view;
     }
 
 }
--- app/src/main/java/com/example/admin/ch8_3/MainActivity.java
+++ app/src/main/java/com/example/admin/ch8_3/MainActivity.java
@@ -9,17 +9,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EditNameDialog.EditNameDialogListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //將btn_fragment1的按鈕綁上監聽物件 btn_fragment1_Click
         Button btn_fragment1 = (Button)findViewById(R.id.btn_fragment1);
         btn_fragment1.setOnClickListener(btn_fragment1_Click);
+        //將btn_editname的按鈕綁上監聽物件 btn_EditName_Click
+        Button btn_editname = (Button)findViewById(R.id.btn_editname);
+        btn_editname.setOnClickListener(btn_EditName_Click);
     }
-    //btn_fragment1_Click的監聽事件
+    //-------MyDialog
+    //設定主畫面btn_fragment1的按鈕點擊監聽事件
     View.OnClickListener btn_fragment1_Click = new View.OnClickListener(){
         public  void  onClick(View v){
             MyDialog dlg = MyDialog.newInstance("確認");
@@ -38,4 +42,17 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "按下取消鈕", Toast.LENGTH_SHORT).show();
         finish();
     }
+
+    //------EditNameDialog
+    public void onFinishEditDialog(String inputText){
+        Toast.makeText(this, "您好,"+inputText, Toast.LENGTH_SHORT).show();
+    }
+
+    View.OnClickListener btn_EditName_Click = new View.OnClickListener(){
+        public  void  onClick(View v){
+            EditNameDialog dlg = new EditNameDialog();
+            FragmentManager fm = getSupportFragmentManager();
+            dlg.show(fm , "EditNameDialog");
+        }
+    };
 }
