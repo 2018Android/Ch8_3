2e14d34b6562cfce17e36308b7f06707302fdaa7
chenyirou <annie851216@gmail.com>
2018-05-22 10:26:47 +0800

    建立Dialog被呼叫會做的事

--- app/src/main/java/com/example/admin/ch8_3/MyDialog.java
+++ app/src/main/java/com/example/admin/ch8_3/MyDialog.java
@@ -1,9 +1,12 @@
 package com.example.admin.ch8_3;
 
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,28 @@ public class MyDialog extends DialogFragment {
         args.putString("title" , title);
         dlg.setArguments(args);
         return dlg;
+    }
+
+    DialogInterface.OnClickListener p_click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialogInterface, int whichButton){
+
+        }
+    };
+
+    DialogInterface.OnClickListener n_click = new DialogInterface.OnClickListener(){
+        public void onClick(DialogInterface dialogInterface, int whichButton){
+
+        }
+    };
+
+    public Dialog onCreateDialog(Bundle saveInstanceState){ //建立Dialog被呼叫會做的事
+        String title = getArguments().getString("title");
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(title)
+                .setMessage("Close Program?")
+                .setPositiveButton("OK", p_click)
+                .setNegativeButton("Cancel", n_click);
+        return builder.create();
     }
 
 
